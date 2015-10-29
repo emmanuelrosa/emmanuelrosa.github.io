933655a52c8e8e22b7b54e24cad820f2462f47b7
Emmanuel Rosa <emmanuel_rosa@aol.com>
2015-10-29 14:04:24 -0400

    build ed28291017d6

---
+++ config.rb
@@ -0,0 +1,20 @@
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_path = "/Users/emmanuelrosa/projects/emmanuelrosa.com/.cache/compass/stylesheets"
+
+# Local development paths
+sass_path = "/Users/emmanuelrosa/projects/emmanuelrosa.com/theme/sass"
+generated_images_path = "/Users/emmanuelrosa/projects/emmanuelrosa.com/.cache/compass/images"
+images_path = "/Users/emmanuelrosa/projects/emmanuelrosa.com/theme/images"
+fonts_path = "/Users/emmanuelrosa/projects/emmanuelrosa.com/theme/fonts"
+
+line_comments = false
+output_style = :compressed
